c3e6232dc042dbc967ba3553e1bc962d18fde05e
cedlemo <cedlemo@gmx.com>
2013-03-26 17:51:09 +0100

    add value_tox_box.lua which was forgotten

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ return
   superproperties = require("blingbling.superproperties");
   helpers = require("blingbling.helpers");
   value_text_box = require("blingbling.value_text_box");
-  --text_box =require("blingbling.text_box");
   line_graph = require("blingbling.line_graph");
   progress_graph = require("blingbling.progress_graph");
   triangular_progressgraph = require("blingbling.triangular_progress_graph");
