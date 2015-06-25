878e69718c8c3bd8ab1f7bd530d447cbb65579f6
cedlemo <cedlemo@gmx.com>
2015-06-25 18:54:12 +0200

    Fix typo in init.lua

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ return
   value_text_box = require("blingbling.value_text_box");
   line_graph = require("blingbling.line_graph");
   progress_graph = require("blingbling.progress_graph");
-  triangular_progressgraph = require("blingbling.triangular_progress_graph");
+  triangular_progress_graph = require("blingbling.triangular_progress_graph");
   volume = require("blingbling.volume");
   popups = require("blingbling.popups");
   net = require("blingbling.net");
